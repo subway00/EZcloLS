34b09d6f66858e46200bc81372b9dd8f952044a3
subway <pig945513@gmail.com>
2019-07-26 15:33:10 +0800

    STABLE VERSION!

--- EZcloLS/src/java/FileManager/AbleTestController.java
+++ EZcloLS/src/java/FileManager/AbleTestController.java
@@ -44,12 +44,11 @@ public class AbleTestController extends HttpServlet {
                     Connection con = DriverManager.getConnection(dbcm.getUrl(), dbcm.getUser(), dbcm.getPw());
                     PreparedStatement pstmt = con.prepareStatement(searchfile);
                     PreparedStatement pstmt2 = con.prepareStatement(searchtestINF);) {
-                
+
                 //get sesssion
-                int F_Number = 0;
-                F_Number = (Integer) request.getSession().getAttribute("choosefile");
-                sessionGetSet(F_Number);
-                
+                // int F_Number = 0;
+//                F_Number = (Integer) request.getSession().getAttribute("choosefile");
+//                sessionGetSet(F_Number);
                 String selectfile = request.getParameter("clickfile");
                 pstmt.setString(1, selectfile);
                 ResultSet result = pstmt.executeQuery();
